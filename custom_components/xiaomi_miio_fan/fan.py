27532b6dc62c72cd913f1e24057dd75954eb8c7e
Sebastian Muszynski <basti@linkt.de>
2019-06-02 18:03:49 +0200

    Add zhimi.fan.za3 support (Closes: #31)

--- custom_components/xiaomi_miio_fan/fan.py
+++ custom_components/xiaomi_miio_fan/fan.py
@@ -32,13 +32,21 @@ MODEL_FAN_V2 = 'zhimi.fan.v2'
 MODEL_FAN_V3 = 'zhimi.fan.v3'
 MODEL_FAN_SA1 = 'zhimi.fan.sa1'
 MODEL_FAN_ZA1 = 'zhimi.fan.za1'
+MODEL_FAN_ZA3 = 'zhimi.fan.za3'
 MODEL_FAN_ZA4 = 'zhimi.fan.za4'
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_HOST): cv.string,
     vol.Required(CONF_TOKEN): vol.All(cv.string, vol.Length(min=32, max=32)),
     vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
-    vol.Optional(CONF_MODEL): vol.In([MODEL_FAN_V2, MODEL_FAN_V3, MODEL_FAN_SA1, MODEL_FAN_ZA1, MODEL_FAN_ZA4]),
+    vol.Optional(CONF_MODEL): vol.In([
+        MODEL_FAN_V2,
+        MODEL_FAN_V3,
+        MODEL_FAN_SA1,
+        MODEL_FAN_ZA1,
+        MODEL_FAN_ZA3,
+        MODEL_FAN_ZA4,
+    ]),
 })
 
 ATTR_MODEL = 'model'
@@ -199,7 +207,7 @@ async def async_setup_platform(hass, config, async_add_devices,
         from miio import Fan
         fan = Fan(host, token, model=model)
         device = XiaomiFan(name, fan, model, unique_id)
-    elif model == MODEL_FAN_ZA4:  # FIXME: Can be removed with python-miio 0.4.6
+    elif model in [MODEL_FAN_ZA3, MODEL_FAN_ZA4]:  # FIXME: Can be removed with python-miio 0.4.6
             from miio import Fan
             fan = Fan(host, token, model=MODEL_FAN_ZA1)
             device = XiaomiFan(name, fan, model, unique_id)
